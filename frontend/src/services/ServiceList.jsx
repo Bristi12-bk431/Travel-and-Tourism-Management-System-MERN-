55a7fba8411876defe74249069a8604c90c17d64
Bristi12-bk431 <bristi.kundu12@gmail.com>
2025-07-28 16:59:05 +0530

    Add Initial File

---
+++ frontend/src/services/ServiceList.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import ServiceCard from './ServiceCard'
+// eslint-disable-next-line no-unused-vars
+import{ Col } from "reactstrap"
+
+import weatherImg from '../assets/images/weather.png'
+import guideImg from '../assets/images/guide.png'
+import customizationImg from '../assets/images/customization.png'
+
+const servicesData =[
+    {
+        imgUrl: weatherImg,
+        title: "Calculate weather",
+        desc: "Get accurate weather forecasts for your travel dates so you can plan better and pack smart.",
+    },
+    {
+         imgUrl: guideImg,
+        title: "Best Tour Guide",
+        desc: "Connect with experienced local guides who bring destination to life with fascinating stories and insights.",
+
+    },
+    {
+         imgUrl: customizationImg,
+        title: "Customization",
+        desc: "Tailor your trip exactly how you want it from luxury stays to offbeat adventures,we've got you covered.",
+    },
+]
+
+const ServiceList = () => {
+  return (
+    <>
+    {servicesData.map((item, index) => (
+        <Col lg="3" key={index}>
+            <ServiceCard item={item} />
+        </Col>
+    ))}
+    </>
+
+    
+  );
+};
+
+export default ServiceList
