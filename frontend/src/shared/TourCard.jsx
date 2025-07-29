765f90a7d40df41a863290f713e9f40d9eddd15f
Koushiki-Chakraborty <koushikichakraborty122003@gmail.com>
2025-07-29 08:31:58 +0530

    some underscore changes

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -8,9 +8,9 @@ const TourCard = ({ tour }) => {
     const { id, title, city, photo, price, featured, avgRating, reviews } = tour;
 
   return (
-    <div className= "tour_card">
+    <div className= "tour__card">
         <Card>
-            <div className= "tour_img">
+            <div className= "tour__img">
                 <img src={photo} alt="tour-img" />
                 <span>Featured</span>
              </div>
