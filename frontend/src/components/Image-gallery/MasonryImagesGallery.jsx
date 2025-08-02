e89f8171bdae4a7c31caf02759a292724c3dce9e
Ayushi1703638 <sumaneshmaity1408@gmail.com>
2025-08-02 14:44:17 +0530

    Part1 done

--- frontend/src/components/Image-gallery/MasonryImagesGallery.jsx
+++ frontend/src/components/Image-gallery/MasonryImagesGallery.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import galleryImages from './galleryimages'
+import galleryimages from './galleryimages'
 
 import Masonry, { ResponsiveMasonry  } from 'react-responsive-masonry'
 
